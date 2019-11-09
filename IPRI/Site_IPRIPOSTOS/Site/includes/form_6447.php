01f74a06ee572f70e1967133286f778caa5cd410
rofarias <rofarias@gmail.com>
2019-11-08 22:59:07 -0300

    Texto corrigido
    Texto corrigido.

--- IPRI/Site_IPRIPOSTOS/Site/includes/form_6447.php
+++ IPRI/Site_IPRIPOSTOS/Site/includes/form_6447.php
@@ -1,14 +1,17 @@
 <?php	
+	if(empty($_POST['']) && strlen($_POST['']) == 0)
+	{
+		return false;
+	}
 	
-	
-	$textarea_2963 = $_POST['textarea_2963'];
+	$ = $_POST[''];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from a Blocs website.";
 	$email_body = "You have received a new message. \n\n".
-				  "Textarea_2963: $textarea_2963 \n";
+				  ": $ \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: DoNotReply@yoursite.com";	
